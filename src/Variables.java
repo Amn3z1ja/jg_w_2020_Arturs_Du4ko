692296a5f28eba9bd8e658c74f335082183da0ae
Amn3z1ja <37236349+Amn3z1ja@users.noreply.github.com>
2020-01-28 13:45:15 +0200

    javaguru commit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String [] args ) {
+        int myInt = 557;
+        long myLong = 44444444;
+        short myShort = 333;
+
+        double myDouble = 123.44;
+        float myFloat = 123.5f;
+        byte myByte = 100; // 128-127
+        System.out.println(myInt);
+        System.out.println(myShort);
+    }
+}
